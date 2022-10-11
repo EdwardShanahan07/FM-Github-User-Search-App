5f3cf4c1314a3d02b65721d6d2cb622208b7b705
Edward Shanahan <edwardshanahan357@gmail.com>
2022-10-11 22:35:34 +0100

    Add Toggle Mode Function

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,1 +1,19 @@
+// Toggle Light Mode Function
 
+const toggleLightMode = () => {
+  const htmlElement = document.querySelector("html");
+
+  document.querySelector("html").classList.toggle("light-mode");
+
+  if (htmlElement.classList.contains("light-mode")) {
+    document.querySelector(".toggle__text").textContent = "dark";
+    document.querySelector(".toggle__icon--sun").style.display = "none";
+    document.querySelector(".toggle__icon--moon").style.display = "block";
+  } else {
+    document.querySelector(".toggle__text").textContent = "light";
+    document.querySelector(".toggle__icon--sun").style.display = "block";
+    document.querySelector(".toggle__icon--moon").style.display = "none";
+  }
+};
+
+document.querySelector(".toggle").addEventListener("click", toggleLightMode);
